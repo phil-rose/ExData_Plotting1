68d0c48dd06668c600227f0017bb9f7b121ee2eb
Phil_Rose <phil.rose233@gmail.com>
2017-05-26 13:33:56 -0500

    First commit

--- Plot3.R
+++ Plot3.R
@@ -1,3 +1,25 @@
+#Read in the data
+data <- read.csv("C:/Users/Phil/Documents/Data_Science/Exploratory_Data_Analysis/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+
+data$Date <- as.character(data$Date)
+
+#Format the Date column as a date
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#Create a subset of the data, subset, for the two dates we will examine, 2-1-2007 and 2-2-2007
+subset <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+#Combine Date and Time column
+date_time <- paste(subset$Date, subset$Time)
+
+#Add the Date_Time column to the subset dataset
+subset <- cbind(date_time, subset)
+
+#Format the new column
+subset$date_time <- as.POSIXct(date_time)
+
+par(mfrow = c(1,1))
+
 #Plot3
 plot(subset$Sub_metering_1~subset$date_time, type = "l", xlab = "", ylab = "Energy sub metering") 
 lines(subset$Sub_metering_2~subset$date_time, col="red")
--- Plot4.R
+++ Plot4.R
@@ -1,3 +1,23 @@
+#Read in the data
+data <- read.csv("C:/Users/Phil/Documents/Data_Science/Exploratory_Data_Analysis/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+
+data$Date <- as.character(data$Date)
+
+#Format the Date column as a date
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#Create a subset of the data, subset, for the two dates we will examine, 2-1-2007 and 2-2-2007
+subset <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+#Combine Date and Time column
+date_time <- paste(subset$Date, subset$Time)
+
+#Add the Date_Time column to the subset dataset
+subset <- cbind(date_time, subset)
+
+#Format the new column
+subset$date_time <- as.POSIXct(date_time)
+
 #Plot4
 par(mfrow = c(2,2))
 plot(subset$Global_active_power~subset$date_time, type = "l", xlab = "", ylab = "Global Active Power")
